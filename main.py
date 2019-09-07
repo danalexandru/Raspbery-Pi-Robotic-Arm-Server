danalexandru/Raspbery-Pi-Robotic-Arm-Server

commit 586f1a5e622dc71b70e55fbcb8e382a37528cdfc

added the "atexit" module in order to stop the cherrypy server at file exit.

main.py
@@ -1,5 +1,7 @@
 # region imports
 import cherrypy
+import atexit
+import sys
 
 from globals import *
 
@@ -37,6 +39,13 @@ def main():
     except Exception as error_message:
         console_log(error_message, LOG_ERROR, main.__name__)
         return False
+
+
+@atexit.register
+def at_exit_file():
+    console_log('The cherrypy server has been exited.')
+    cherrypy.engine.stop()
+    sys.exit(0)
 
 
 if __name__ == "__main__":

commit 1704d152068bc851a6afbafad7ab117ed4822617

fixed errors caused by the "586f1a5e" commit

main.py
@@ -43,9 +43,9 @@ def main():
 
 @atexit.register
 def at_exit_file():
-    console_log('The cherrypy server has been exited.')
+    console_log('The cherrypy server has been shut down.', LOG_SUCCESS)
     cherrypy.engine.stop()
-    sys.exit(0)
+    cherrypy.engine.exit()
 
 
 if __name__ == "__main__":