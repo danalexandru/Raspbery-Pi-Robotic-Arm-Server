danalexandru/Raspbery-Pi-Robotic-Arm-Server

commit a332eb30c8f7facbb7aa98abfb3abe83511010e3

- added documentation for every python file in the project - updated "debug.py" to keep track of the servo position and duty cycle

debug.py
@@ -1,3 +1,8 @@
+"""
+This file is a debug file used in order to test the servo motor
+"""
+
+
 # region imports
 # noinspection PyUnresolvedReferences
 import Rpi.GPIO as GPIO
@@ -92,7 +97,9 @@ class PwmHandler(object):
 
             duty_cycle = (new_position * 100) / pulse_width_time_period
             self.my_pwm.ChangeDutyCycle(duty_cycle)
-
+            console_log('Duty cycle = %s) new_position = %s' % (str(self.duty_cycle), str(new_position)),
+                        LOG_SUCCESS,
+                        self.set_my_pwm_duty_cycle.__name__)
             return True
         except Exception as error_message:
             console_log(error_message, LOG_ERROR, self.set_my_pwm_duty_cycle.__name__)
@@ -119,7 +126,7 @@ def main():
         pwm_handler.start_my_pwm()
         while True:
             # keyboard_input = input('Increment / Decrement servo? (\u2191, \u2193)')
-            keyboard_input = input('Increment / Decrement servo? (w, s)')
+            keyboard_input = input('%s\t Increment / Decrement servo? (w, s): %s' % (CODE_BLUE, CODE_WHITE))
 
             keyboard_input = keyboard_input.lower()
             console_log(keyboard_input, LOG_INFO, main.__name__)

servo.py
@@ -1,3 +1,7 @@
+"""
+This file has all the methods used to control the servo motors (including all the GPIO handler)
+"""
+
 # region imports
 # noinspection PyUnresolvedReferences
 import Rpi.GPIO as GPIO

globals.py
@@ -1,3 +1,7 @@
+"""
+This file has flags and methods that are used all throwout the project
+"""
+
 # region global variables
 
 

main.py
@@ -1,3 +1,8 @@
+"""
+This file is the main part of the Project. This is the file that is being executed at the start of the cherrypy server
+"""
+
+
 # region imports
 import cherrypy
 import atexit
@@ -12,7 +17,6 @@ from rest import ExitCherryPyServer
 
 
 # region main
-
 def main():
     try:
         cherrypy.engine.exit()
@@ -43,7 +47,7 @@ def main():
 
 @atexit.register
 def at_exit_file():
-    console_log('The cherrypy server has been shut down.', LOG_SUCCESS)
+    console_log('The cherrypy server has been shut down.', LOG_SUCCESS, at_exit_file.__name__)
     cherrypy.engine.stop()
     cherrypy.engine.exit()
 

rest.py
@@ -1,3 +1,7 @@
+"""
+Description: This file has the REST API functionality
+"""
+
 # region imports
 from unittest import loader
 

commit f851df6038f305d5ae0e681b708f1335f90f36a1

- fixed errors introduced in commit "a332eb30"

Makefile
@@ -4,3 +4,6 @@ output:
 
 clean:
 	rm *.pyc
+
+debug:
+	sudo python3 debug.py

debug.py
@@ -5,7 +5,7 @@ This file is a debug file used in order to test the servo motor
 
 # region imports
 # noinspection PyUnresolvedReferences
-import Rpi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import atexit
 from globals import *
 # endregion imports
@@ -19,15 +19,15 @@ class PwmHandler(object):
     def __init__(self):
         try:
             self.my_pwm = None
-            self.pin = 0
-            self.frequency = 0
+            self.pin = 11
+            self.frequency = 50
             self.lower_limit = 1
             self.upper_limit = 2
             self.step = 0.01
 
             pulse_width_time_period = 1000 / self.frequency
             self.duty_cycle = (((self.lower_limit + self.upper_limit) / 2) * 100) / pulse_width_time_period
-
+            console_log('duty cycle = %s' % str(self.duty_cycle), LOG_INFO, self.__init__.__name__)
             return
         except Exception as error_message:
             console_log(error_message, LOG_ERROR, self.__init__.__name__)
@@ -96,6 +96,8 @@ class PwmHandler(object):
                 return False
 
             duty_cycle = (new_position * 100) / pulse_width_time_period
+            self.duty_cycle = duty_cycle
+
             self.my_pwm.ChangeDutyCycle(duty_cycle)
             console_log('Duty cycle = %s) new_position = %s' % (str(self.duty_cycle), str(new_position)),
                         LOG_SUCCESS,
@@ -138,7 +140,7 @@ def main():
                 continue
 
     except Exception as error_message:
-        console_log(error_message, LOG_WARNING, main.__name__)
+        console_log(error_message, LOG_ERROR, main.__name__)
         return False
 
 

servo.py
@@ -4,7 +4,7 @@ This file has all the methods used to control the servo motors (including all th
 
 # region imports
 # noinspection PyUnresolvedReferences
-import Rpi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 from globals import *
 # endregion imports