danalexandru/Raspbery-Pi-Robotic-Arm-Server

commit a332eb30c8f7facbb7aa98abfb3abe83511010e3

- added documentation for every python file in the project - updated "debug.py" to keep track of the servo position and duty cycle

debug.py
@@ -1,3 +1,8 @@
+"""
+This file is a debug file used in order to test the servo motor
+"""
+
+
 # region imports
 # noinspection PyUnresolvedReferences
 import Rpi.GPIO as GPIO
@@ -92,7 +97,9 @@ class PwmHandler(object):
 
             duty_cycle = (new_position * 100) / pulse_width_time_period
             self.my_pwm.ChangeDutyCycle(duty_cycle)
-
+            console_log('Duty cycle = %s) new_position = %s' % (str(self.duty_cycle), str(new_position)),
+                        LOG_SUCCESS,
+                        self.set_my_pwm_duty_cycle.__name__)
             return True
         except Exception as error_message:
             console_log(error_message, LOG_ERROR, self.set_my_pwm_duty_cycle.__name__)
@@ -119,7 +126,7 @@ def main():
         pwm_handler.start_my_pwm()
         while True:
             # keyboard_input = input('Increment / Decrement servo? (\u2191, \u2193)')
-            keyboard_input = input('Increment / Decrement servo? (w, s)')
+            keyboard_input = input('%s\t Increment / Decrement servo? (w, s): %s' % (CODE_BLUE, CODE_WHITE))
 
             keyboard_input = keyboard_input.lower()
             console_log(keyboard_input, LOG_INFO, main.__name__)

globals.py
@@ -1,3 +1,7 @@
+"""
+This file has flags and methods that are used all throwout the project
+"""
+
 # region global variables
 
 

main.py
@@ -1,3 +1,8 @@
+"""
+This file is the main part of the Project. This is the file that is being executed at the start of the cherrypy server
+"""
+
+
 # region imports
 import cherrypy
 import atexit
@@ -12,7 +17,6 @@ from rest import ExitCherryPyServer
 
 
 # region main
-
 def main():
     try:
         cherrypy.engine.exit()
@@ -43,7 +47,7 @@ def main():
 
 @atexit.register
 def at_exit_file():
-    console_log('The cherrypy server has been shut down.', LOG_SUCCESS)
+    console_log('The cherrypy server has been shut down.', LOG_SUCCESS, at_exit_file.__name__)
     cherrypy.engine.stop()
     cherrypy.engine.exit()
 

rest.py
@@ -1,3 +1,7 @@
+"""
+Description: This file has the REST API functionality
+"""
+
 # region imports
 from unittest import loader
 

servo.py
@@ -1,3 +1,7 @@
+"""
+This file has all the methods used to control the servo motors (including all the GPIO handler)
+"""
+
 # region imports
 # noinspection PyUnresolvedReferences
 import Rpi.GPIO as GPIO

commit 7eefeb3bf7d38ff04c780cdbc12f3574db0feb61

- fixed errors introduced in the "a332eb30" commit - added the directories "images" and "camera"

camera.py
@@ -59,14 +59,14 @@ class PiCameraHandler(object):
     def start_recording(self):
         try:
             self.set_entity_name(self.file_format.VIDEO)
-            self.camera_handler.start_recording()
+            self.camera_handler.start_recording(self.entity_name)
         except Exception as error_message:
             console_log(error_message, LOG_ERROR, self.start_recording.__name__)
             return False
 
     def stop_recording(self):
         try:
-            self.camera_handler.stoprecording()
+            self.camera_handler.stop_recording()
         except Exception as error_message:
             console_log(error_message, LOG_ERROR, self.stop_recording.__name__)
             return False
@@ -78,6 +78,7 @@ class PiCameraHandler(object):
         except Exception as error_message:
             console_log(error_message, LOG_ERROR, self.set_recording_time_frame.__name__)
             return False
+
 
 pi_camera_handler = PiCameraHandler()
 # endregion PiCameraHandler

debug.py
@@ -189,7 +189,7 @@ def main():
         if keyboard_input == 'servo':
             servo()
         elif keyboard_input == 'camera':
-            pass
+            camera()
         else:
             return False
 
@@ -209,7 +209,6 @@ def at_exit_file():
     try:
         console_log('The debug file has been exited successfully.', LOG_SUCCESS, at_exit_file.__name__)
         pwm_handler.stop_my_pwm()
-
         return True
     except Exception as error_message:
         console_log(error_message, LOG_ERROR, at_exit_file.__name__)

images/desktop.ini
@@ -1,0 +1,5 @@
+[.ShellClassInfo]
+InfoTip=This folder is shared online.
+IconFile=C:\Program Files\Google\Drive\googledrivesync.exe
+IconIndex=16
+    

videos/desktop.ini
@@ -1,0 +1,5 @@
+[.ShellClassInfo]
+InfoTip=This folder is shared online.
+IconFile=C:\Program Files\Google\Drive\googledrivesync.exe
+IconIndex=16
+    